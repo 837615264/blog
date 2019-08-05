d977bbfb5a629573cc9e49c67f0d4555d700b86b
837615264 <837615264@qq.com>
2019-08-05 16:08:15 +0800

    备案信息变更

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://101.37.67.249/';
+$config['base_url'] = 'http://www.51neko.com/';
 
 /*
 |--------------------------------------------------------------------------
